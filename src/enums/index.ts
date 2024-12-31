74b6c67af100d77a889ed77e37949ae41170d4e7
botuns <abdulqaharolajide@gmail.com>
2024-12-31 08:45:27 +0100

    add enums for user types and attendance types

---
+++ src/enums/index.ts
@@ -0,0 +1,13 @@
+// enums for user type
+export enum UserType {
+  ADMIN = "ADMIN",
+  OFFICER = "OFFICER",
+  GUEST = "GUEST",
+}
+
+// enum for attendance type
+export enum AttendanceType {
+  FINGERPRINT = "FINGERPRINT",
+  UNIQUE_CODE = "UNIQUE_CODE",
+  GUEST_DETAILS = "GUEST_DETAILS",
+}
