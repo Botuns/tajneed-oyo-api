ceb42ebe4a3f407e38b812af5a7b6ae6d8c4aa87
botuns <abdulqaharolajide@gmail.com>
2024-12-31 09:33:25 +0100

    add OfficeMail type and initial officersMails array

---
+++ src/lib/offices-mails.ts
@@ -0,0 +1,12 @@
+// conatins all the mails of the offices
+// each office has a mail and type
+export type OfficeMail = {
+  mail: string;
+  office: string;
+};
+export const officersMails: OfficeMail[] = [
+  {
+    mail: "",
+    office: "",
+  },
+];
