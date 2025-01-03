2f12256119c61ec961de37fbb6deecfd2946c567
botuns <abdulqaharolajide@gmail.com>
2025-01-03 08:22:27 +0100

    add express-validator package and update office routes to enable request validation

--- src/routes/office.routes.ts
+++ src/routes/office.routes.ts
@@ -1,44 +1,44 @@
-// import { Router } from "express";
-// import { OfficeController } from "../controllers/office.controller";
+import { Router } from "express";
+import { OfficeController } from "../controllers/office.controller";
 
-// import { Logger } from "../utils/logger";
-// import { AddOfficerToOfficeDto, CreateOfficeDto, UpdateOfficeDto } from "../lib/types/DTOs";
+import { Logger } from "../utils/logger";
+import { AddOfficerToOfficeDto, CreateOfficeDto, UpdateOfficeDto } from "../lib/types/DTOs";
 
-// export const officeRouter = Router();
-// const officeController = new OfficeController();
-// const logger = new Logger("OfficeRoutes");
+export const officeRouter = Router();
+const officeController = new OfficeController();
+const logger = new Logger("OfficeRoutes");
 
-// officeRouter.use(isAuthenticated);
+officeRouter.use(isAuthenticated);
 
-// officeRouter.use((req, res, next) => {
-//   logger.info("Incoming office route request", {
-//     method: req.method,
-//     path: req.path,
-//     query: req.query,
-//     body: req.body,
-//   });
-//   next();
-// });
+officeRouter.use((req, res, next) => {
+  logger.info("Incoming office route request", {
+    method: req.method,
+    path: req.path,
+    query: req.query,
+    body: req.body,
+  });
+  next();
+});
 
-// officeRouter.get("/", officeController.getAllOffices);
-// officeRouter.get("/:id", officeController.getOfficeById);
+officeRouter.get("/", officeController.getAllOffices);
+officeRouter.get("/:id", officeController.getOfficeById);
 
-// officeRouter.post(
-//   "/",
-//   isAdmin,
-//   validateRequest(CreateOfficeDto),
-//   officeController.createOffice
-// );
-// officeRouter.patch(
-//   "/:id",
-//   isAdmin,
-//   validateRequest(UpdateOfficeDto),
-//   officeController.updateOffice
-// );
-// officeRouter.delete("/:id", isAdmin, officeController.deleteOffice);
-// officeRouter.post(
-//   "/:id/officers",
-//   isAdmin,
-//   validateRequest(AddOfficerToOfficeDto),
-//   officeController.addOfficerToOffice
-// );
+officeRouter.post(
+  "/",
+  isAdmin,
+  validateRequest(CreateOfficeDto),
+  officeController.createOffice
+);
+officeRouter.patch(
+  "/:id",
+  isAdmin,
+  validateRequest(UpdateOfficeDto),
+  officeController.updateOffice
+);
+officeRouter.delete("/:id", isAdmin, officeController.deleteOffice);
+officeRouter.post(
+  "/:id/officers",
+  isAdmin,
+  validateRequest(AddOfficerToOfficeDto),
+  officeController.addOfficerToOffice
+);
